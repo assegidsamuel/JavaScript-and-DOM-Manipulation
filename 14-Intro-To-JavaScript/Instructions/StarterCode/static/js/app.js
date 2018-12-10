7e665ccc1aab2187a3101a99896734bc65023417
assegidsamuel <41666029+assegidsamuel@users.noreply.github.com>
2018-12-09 22:50:02 -0500

    Add files via upload

---
+++ 14-Intro-To-JavaScript/Instructions/StarterCode/static/js/app.js
@@ -0,0 +1,16 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+var $tbody = tableData.querySelector("tbody");
+
+var $searchBtn = tableData.querySelector("#filter-btn");
+
+
+
+
+$searchBtn.addEventListener("click", handleSearchButtonClick);
+
+
+
+
