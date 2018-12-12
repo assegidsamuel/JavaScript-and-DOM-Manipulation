8f2f6ad3e2b8a59890596e679ab11f7001881c39
assegidsamuel <41666029+assegidsamuel@users.noreply.github.com>
2018-12-12 16:33:45 -0500

    Add files via upload

--- 14-Intro-To-JavaScript/Instructions/StarterCode/static/js/app.js
+++ 14-Intro-To-JavaScript/Instructions/StarterCode/static/js/app.js
@@ -2,15 +2,35 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-var $tbody = tableData.querySelector("tbody");
+var row = {
+  datetime: "1/6/2010",
+  city: "san diego",
+  state: "ca",
+  country: "us",
+  shape: "light",
+  durationMinutes: "20 minutes",
+  comments: "Unusual silent&#44 orange glowing light stopping changing direction and discharging sudstance."
+}
 
-var $searchBtn = tableData.querySelector("#filter-btn");
+function filtertable () {
+d3.event.preventDefault();
+
+var tbody = d3.select("tbody")
+
+tableData.forEach(row => {
+  tr = tbody.append('tr')
+
+  td = tr.append('td').text(row.datetime)
+  td = tr.append('td').text(row.city)
+  td = tr.append('td').text(row.state)
+  td = tr.append('td').text(row.country)
+  td = tr.append('td').text(row.shape)
+  td = tr.append('td').text(row.durationMinutes)
+  td = tr.append('td').text(row.comments)
+
+})}
 
 
-
-
-$searchBtn.addEventListener("click", handleSearchButtonClick);
-
-
+d3.select('#filter-btn').on('click', filtertable)
 
 
